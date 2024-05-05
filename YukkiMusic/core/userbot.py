1604ad02b4f8ae1ab89b4424891053e1d4f34253
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2024-05-05 07:05:14 +0700

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -1,11 +1,12 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
+#
 
 import sys
 
@@ -22,33 +23,38 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
+            name="YukkiOne",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING1),
+            session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
+            name="YukkiTwo",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING2),
+            session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
+            name="YukkiThree",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING3),
+            session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
+            name="YukkiFour",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING4),
+            session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
+            name="YukkiFive",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING5),
+            session_string=str(config.STRING5),
             no_updates=True,
         )
 
@@ -57,9 +63,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYM")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("AdityaServer")
+                await self.one.join_chat("AdityaSource")
+                await self.one.join_chat("AdityaDiscus")
             except:
                 pass
             assistants.append(1)
@@ -88,9 +94,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TeamYM")
-                await self.two.join_chat("TheYukki")
-                await self.two.join_chat("YukkiSupport")
+                await self.two.join_chat("AdityaServer")
+                await self.two.join_chat("AdityaSource")
+                await self.two.join_chat("AdityaDiscus")
             except:
                 pass
             assistants.append(2)
@@ -119,9 +125,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TeamYM")
-                await self.three.join_chat("TheYukki")
-                await self.three.join_chat("YukkiSupport")
+                await self.three.join_chat("AdityaServer")
+                await self.three.join_chat("AdityaSource")
+                await self.three.join_chat("AdityaDiscus")
             except:
                 pass
             assistants.append(3)
@@ -150,9 +156,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TeamYM")
-                await self.four.join_chat("TheYukki")
-                await self.four.join_chat("YukkiSupport")
+                await self.four.join_chat("AdityaServer")
+                await self.four.join_chat("AdityaSource")
+                await self.four.join_chat("AdityaDiscus")
             except:
                 pass
             assistants.append(4)
@@ -181,9 +187,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TeamYM")
-                await self.five.join_chat("TheYukki")
-                await self.five.join_chat("YukkiSupport")
+                await self.five.join_chat("AdityaServer")
+                await self.five.join_chat("AdityaSource")
+                await self.five.join_chat("AdityaDiscus")
             except:
                 pass
             assistants.append(5)
@@ -208,4 +214,220 @@ class Userbot(Client):
                 self.five.name = get_me.first_name
             LOGGER(__name__).info(
                 f"Assistant Five Started as {self.five.name}"
+        )#
+# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
+#
+# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+import sys
+
+from pyrogram import Client
+
+import config
+
+from ..logging import LOGGER
+
+assistants = []
+assistantids = []
+
+
+class Userbot(Client):
+    def __init__(self):
+        self.one = Client(
+            name="YukkiOne",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING1),
+            no_updates=True,
+        )
+        self.two = Client(
+            name="YukkiTwo",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING2),
+            no_updates=True,
+        )
+        self.three = Client(
+            name="YukkiThree",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING3),
+            no_updates=True,
+        )
+        self.four = Client(
+            name="YukkiFour",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING4),
+            no_updates=True,
+        )
+        self.five = Client(
+            name="YukkiFive",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING5),
+            no_updates=True,
+        )
+
+    async def start(self):
+        LOGGER(__name__).info(f"Starting Assistant Clients")
+        if config.STRING1:
+            await self.one.start()
+            try:
+                await self.one.join_chat("AdityaServer")
+                await self.one.join_chat("AdityaSource")
+                await self.one.join_chat("AdityaDiscus")
+            except:
+                pass
+            assistants.append(1)
+            try:
+                await self.one.send_message(
+                    config.LOG_GROUP_ID, "Assistant Started"
+                )
+            except:
+                LOGGER(__name__).error(
+                    f"Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+                sys.exit()
+            get_me = await self.one.get_me()
+            self.one.username = get_me.username
+            self.one.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.one.name = (
+                    get_me.first_name + " " + get_me.last_name
+                )
+            else:
+                self.one.name = get_me.first_name
+            LOGGER(__name__).info(
+                f"Assistant Started as {self.one.name}"
             )
+        if config.STRING2:
+            await self.two.start()
+            try:
+                await self.two.join_chat("AdityaServer")
+                await self.two.join_chat("AdityaSource")
+                await self.two.join_chat("AdityaDiscus")
+            except:
+                pass
+            assistants.append(2)
+            try:
+                await self.two.send_message(
+                    config.LOG_GROUP_ID, "Assistant Started"
+                )
+            except:
+                LOGGER(__name__).error(
+                    f"Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+                sys.exit()
+            get_me = await self.two.get_me()
+            self.two.username = get_me.username
+            self.two.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.two.name = (
+                    get_me.first_name + " " + get_me.last_name
+                )
+            else:
+                self.two.name = get_me.first_name
+            LOGGER(__name__).info(
+                f"Assistant Two Started as {self.two.name}"
+            )
+        if config.STRING3:
+            await self.three.start()
+            try:
+                await self.three.join_chat("AdityaServer")
+                await self.three.join_chat("AdityaSource")
+                await self.three.join_chat("AdityaDiscus")
+            except:
+                pass
+            assistants.append(3)
+            try:
+                await self.three.send_message(
+                    config.LOG_GROUP_ID, "Assistant Started"
+                )
+            except:
+                LOGGER(__name__).error(
+                    f"Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+                sys.exit()
+            get_me = await self.three.get_me()
+            self.three.username = get_me.username
+            self.three.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.three.name = (
+                    get_me.first_name + " " + get_me.last_name
+                )
+            else:
+                self.three.name = get_me.first_name
+            LOGGER(__name__).info(
+                f"Assistant Three Started as {self.three.name}"
+            )
+        if config.STRING4:
+            await self.four.start()
+            try:
+                await self.four.join_chat("AdityaServer")
+                await self.four.join_chat("AdityaSource")
+                await self.four.join_chat("AdityaDiscus")
+            except:
+                pass
+            assistants.append(4)
+            try:
+                await self.four.send_message(
+                    config.LOG_GROUP_ID, "Assistant Started"
+                )
+            except:
+                LOGGER(__name__).error(
+                    f"Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+                sys.exit()
+            get_me = await self.four.get_me()
+            self.four.username = get_me.username
+            self.four.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.four.name = (
+                    get_me.first_name + " " + get_me.last_name
+                )
+            else:
+                self.four.name = get_me.first_name
+            LOGGER(__name__).info(
+                f"Assistant Four Started as {self.four.name}"
+            )
+        if config.STRING5:
+            await self.five.start()
+            try:
+                await self.five.join_chat("AdityaServer")
+                await self.five.join_chat("AdityaSource")
+                await self.five.join_chat("AdityaDiscus")
+            except:
+                pass
+            assistants.append(5)
+            try:
+                await self.five.send_message(
+                    config.LOG_GROUP_ID, "Assistant Started"
+                )
+            except:
+                LOGGER(__name__).error(
+                    f"Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+                sys.exit()
+            get_me = await self.five.get_me()
+            self.five.username = get_me.username
+            self.five.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.five.name = (
+                    get_me.first_name + " " + get_me.last_name
+                )
+            else:
+                self.five.name = get_me.first_name
+            LOGGER(__name__).info(
+                f"Assistant Five Started as {self.five.name}"
+        )
